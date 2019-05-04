4e77518033f98d372af690a16e7e4237e196aa5c
Benz Boonsaree <33254030+BenzPowder@users.noreply.github.com>
2019-05-04 17:51:37 +0700

    add UI

---
+++ insertAsset.php
@@ -0,0 +1,94 @@
+<?php
+include "config.php";
+include "sidebar.php";
+?>
+
+<link rel="stylesheet" type="text/css" href="jquery.datetimepicker.css">
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="jquery.datetimepicker.js"></script>
+
+<html>
+<script type="text/javascript">
+function fncSubmit()
+{
+    if(document.getElementById('cNumber').value == "")
+    {
+        alert('กรุณากรอกรหัสวิชา Ex.322444');
+        return false;
+    }
+
+    if(document.getElementById('cName').value == "")
+    {
+        alert('กรุณากรอกชื่อวิชา Ex.Data Science');
+        return false;
+    }
+
+    if(document.getElementById('cYear').value == "")
+    {
+        alert('กรุณากรอกปีการศึกษา Ex.2561');
+        return false;
+    }
+
+    if(document.getElementById('cPassword').value == "")
+    {
+        alert('กรุณากรอกรหัสเข้าร่วม Ex.Dfrt007');
+        return false;
+    }
+}
+</script>
+<body >
+	<div class="container">
+<form  action="add.php" method="post" onSubmit="JavaScript:return fncSubmit();">
+<div class="form-group">
+      <div align="center">
+          <label>นำเข้าครุภัณฑ์</label> <br><br>
+      </div>
+      <label>หมายเลขครุภัณฑ์</label>
+      <input type="text" name="assetNumber" class="form-control" required pattern="[a-zA-Z-0-9\s]*" title="กรุณากรอกตัวเลขและตัวอักษร" id="assetNumber" placeholder="">
+    </div>
+    <div class="form-group">
+      <label>ชื่อครุภัณฑ์</label>
+      <input type="text" name="assetName" class="form-control" required pattern="[a-zA-Zก-ุฯ-๙\s]*" title="กรุณากรอกตัวอักษร" id="assetName" placeholder="">
+    </div>
+    <div class="form-group">
+      <label>สถานที่ตั้ง</label>
+      <input type="text" name="assetLocation" class="form-control" id="assetLocation" required pattern="[0-9\s]{4}" title="กรุณากรอกปีการศึกษา" placeholder="">
+    </div>
+    <div class="form-group">
+      <form name="itoffside" method="POST">
+	<input type="text" name="startdate" value="" id="startdate" />
+	</form>
+	 <script type="text/javascript">
+ jQuery('#startdate').datetimepicker();
+ </script>
+    </div>
+    <div class="form-group">
+      <label>จำนวนเซคชัน</label>
+      <select class="form-control" name="cSection" id="cSection">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label>รหัสเข้าร่วม</label>
+      <input type="text" class="form-control" name="cPassword" id="cPassword" placeholder="">
+    </div>
+    <div class="form-group">
+      <label>สถานะรายวิชา</label>
+      <select class="form-control" name="cStatus" id="cStatus">
+      <option value="0">ปิด</option>
+      <option value="1">เปิด</option>
+      </select>
+    </div>
+    <div align="center">
+      <input type="submit" name="submit" class="btn btn-primary" id="addSubject" value="บันทึก">
+    </div>
+    <br>
+</form>
+</div>
+</body>
+</html>
+
---
+++ sidebar.php
@@ -0,0 +1,275 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Asset Management</title>
+  <link rel="icon" type="image/png" href="https://cdn.icon-icons.com/icons2/1535/PNG/512/3298612-document-paper-sign-signing_107010.png" />
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+
+
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+
+      <!-- Sidebar - Brand -->
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
+        <div class="sidebar-brand-icon">
+          <img src="https://upload.wikimedia.org/wikipedia/th/c/c3/Science_KKU_Thai_Emblem.png" width="50px" height="50px">
+        </div>
+        <div class="sidebar-brand-text mx-3">ระบบจัดการครุภัณฑ์</div>
+      </a>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
+
+      <!-- Nav Item - Dashboard -->
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">
+          <i class="fas fa-fw fa-tachometer-alt"></i>
+          <span>หน้าแรก</span></a>
+      </li>
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Interface
+      </div>
+
+      <li class="nav-item">
+        <a class="nav-link" href="asset.php">
+          <img src="https://images.vexels.com/media/users/3/157256/isolated/preview/444a13e060df58c2af22710355955c07-black-and-while-stacked-books-icon-by-vexels.png" width="20px" height="20px">
+          <span>ครุภัณฑ์ทั้งหมด</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="checkAsset.php">
+          <img src="https://cdn.pixabay.com/photo/2017/01/10/23/01/seo-1970475_960_720.png" width="20px" height="20px">
+          <span>ตรวจสอบครุภัณฑ์</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="insertAsset.php">
+          <img src="http://download.seaicons.com/icons/custom-icon-design/mono-general-2/512/import-icon.png" width="20px" height="20px">
+          <span>นำเข้าครุภัณฑ์</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="borrowAsset.php">
+          <img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/transfer-89-537154.png" width="20px" height="20px">
+          <span>ยืมครุภัณฑ์</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="returnAsset.php">
+          <img src="https://cdn.iconscout.com/icon/premium/png-512-thumb/transfer-89-537154.png" width="20px" height="20px">
+          <span>คืนครุภัณฑ์</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="repairAsset.php">
+          <img src="https://png.pngtree.com/element_our/md/20180314/md_5aa8fcee8db45.png" width="20px" height="20px">
+          <span>แจ้งปัญหาครุภัณฑ์</span></a>
+      </li>
+
+      <!-- Nav Item - Utilities Collapse Menu -->
+
+      <!-- Divider -->
+      <hr class="sidebar-divider">
+
+      <!-- Heading -->
+      <div class="sidebar-heading">
+        Addons
+      </div>
+       <li class="nav-item">
+        <a class="nav-link" href="login.php">
+          <img src="https://png.pngtree.com/svg/20170831/d323f01e9c.png" width="20px" height="20px">
+          <span>Login</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="register.php">
+          <img src="http://cdn.onlinewebfonts.com/svg/img_102074.png" width="20px" height="20px">
+          <span>Register</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="forgot-password.php">
+          <img src="https://png.pngtree.com/svg/20170213/12bf1e389c.svg" width="20px" height="20px">
+          <span>Forget password</span></a>
+      </li>
+
+      <!-- Nav Item - Pages Collapse Menu -->
+
+      <!-- Nav Item - Charts -->
+      <li class="nav-item">
+        <a class="nav-link" href="contactUs.php">
+            <img src="https://png.pngtree.com/svg/20170825/contact_us_711255.png" width="20px" height="20px">
+          <span>Contact Us</span></a>
+      </li>
+
+      <!-- Nav Item - Tables -->
+
+      <!-- Divider -->
+      <hr class="sidebar-divider d-none d-md-block">
+
+      <!-- Sidebar Toggler (Sidebar) -->
+      <div class="text-center d-none d-md-inline">
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+      </div>
+
+    </ul>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+          <!-- Sidebar Toggle (Topbar) -->
+          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+            <i class="fa fa-bars"></i>
+          </button>
+
+          <!-- Topbar Search -->
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="button">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+
+          <!-- Topbar Navbar -->
+          <ul class="navbar-nav ml-auto">
+
+            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+            <li class="nav-item dropdown no-arrow d-sm-none">
+              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-search fa-fw"></i>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                <form class="form-inline mr-auto w-100 navbar-search">
+                  <div class="input-group">
+                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                    <div class="input-group-append">
+                      <button class="btn btn-primary" type="button">
+                        <i class="fas fa-search fa-sm"></i>
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </li>
+
+            <!-- Nav Item - Alerts -->
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-bell fa-fw"></i>
+                <!-- Counter - Alerts -->
+                <span class="badge badge-danger badge-counter">3+</span>
+              </a>
+              <!-- Dropdown - Alerts -->
+              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
+                <h6 class="dropdown-header">
+                  Alerts Center
+                </h6>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                  <div class="mr-3">
+                    <div class="icon-circle bg-primary">
+                      <i class="fas fa-file-alt text-white"></i>
+                    </div>
+                  </div>
+                  <div>
+                    <div class="small text-gray-500">26 เมษายน 2562</div>
+                    <span class="font-weight-bold">ขออนุญาติยืมครุภัณฑ์</span>
+                  </div>
+                </a>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                  <div class="mr-3">
+                  </div>
+                  <div>
+                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+              </div>
+            </li>
+
+            <!-- Nav Item - Messages -->
+            <li class="nav-item dropdown no-arrow mx-1">
+              <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-envelope fa-fw"></i>
+                <!-- Counter - Messages -->
+                <span class="badge badge-danger badge-counter">1</span>
+              </a>
+              <!-- Dropdown - Messages -->
+              <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
+                <h6 class="dropdown-header">
+                  Message Center
+                </h6>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                  <div class="dropdown-list-image mr-3">
+                    <img class="rounded-circle" src="https://source.unsplash.com/fn_BT9fwg_E/60x60" alt="">
+                    <div class="status-indicator bg-success"></div>
+                  </div>
+                  <div class="font-weight-bold">
+                    <div class="text-truncate">แจ้งปัญหาครุภัณฑ์ชำรุด</div>
+                    <div class="small text-gray-500">Emily Fowler · 58m</div>
+                  </div>
+                </a>
+            </li>
+
+            <div class="topbar-divider d-none d-sm-block"></div>
+
+            <!-- Nav Item - User Information -->
+            <li class="nav-item dropdown no-arrow">
+              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">ทดสอบระบบ</span>
+                <img class="img-profile rounded-circle" src="https://cdn.pixabay.com/photo/2014/04/03/09/58/man-309490_960_720.png">
+              </a>
+              <!-- Dropdown - User Information -->
+              <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Profile
+                </a>
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Settings
+                </a>
+                <a class="dropdown-item" href="#">
+                  <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Activity Log
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                  <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Logout
+                </a>
+              </div>
+            </li>
+
+          </ul>
+
+        </nav>
